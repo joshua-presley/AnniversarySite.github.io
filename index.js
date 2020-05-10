b87c2571bd2ba95f54193456f60be70fe3c02598
joshua-presley <58670600+joshua-presley@users.noreply.github.com>
2020-05-10 16:28:36 -0400

    add template for the other three sections add jquery to project make items fade in when you scroll over them

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-/*
-Script for main page
-*/
+$(document).ready(function() {
 
-
-
-console.log("hello");
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.child').each( function(i){
+    
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = window.scrollY + window.innerHeight;
+            console.log(bottom_of_window);
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+    
+                $(this).animate({'opacity':'1'},1000);
+    
+            }    
+        }); 
+    }); 
+});
