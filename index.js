5bdfb7e070bd29c9f48b589e80cf08a7dbeea366
joshua-presley <58670600+joshua-presley@users.noreply.github.com>
2020-06-03 21:16:48 -0400

    make some changes to how the scroll works

--- index.js
+++ index.js
@@ -10,11 +10,10 @@ $(document).ready(function() {
             var bottom_of_window = window.scrollY + window.innerHeight;
             console.log(bottom_of_window);
             /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-    
+            if( bottom_of_window > bottom_of_object){
                 $(this).animate({'opacity':'1'},1000);
-    
-            }    
+            } 
+             
         }); 
     }); 
 });
